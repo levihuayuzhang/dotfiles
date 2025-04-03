ed8801663496c828cf11aebb70c0f165f560ffcb
Huayu Zhang <levihuayuzhang@foxmail.com>
2025-04-03 14:21:30 +0800

    add transparent for everforest

--- nvim/.config/nvim/lua/plugins/everforest.lua
+++ nvim/.config/nvim/lua/plugins/everforest.lua
@@ -2,7 +2,14 @@ return {
     'sainnhe/everforest',
     priority = 1000,
     config = function()
+        vim.g.everforest_transparent_background = 2
+        vim.g.everforest_float_style = 'dim'
+        vim.g.everforest_diagnostic_text_highlight = 1
         vim.g.everforest_diagnostic_line_highlight = 1
-        vim.cmd [[colorscheme everforest]]
+        vim.g.everforest_diagnostic_virtual_text = 'highlighted'
+        vim.g.everforest_dim_inactive_windows = 1
+        vim.g.everforest_ui_contrast = 'high'
+        vim.g.everforest_better_performans = 1
+        vim.cmd.colorscheme 'everforest'
     end,
 }
--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -5,8 +5,8 @@ return {
     config = function()
         require('gruvbox').setup {
             contrast = 'hard', -- can be "hard", "soft" or empty string
-            -- transparent_mode = true,
-            transparent_mode = false,
+            transparent_mode = true,
+            -- transparent_mode = false,
         }
         vim.o.background = 'dark' -- or "light" for light mode
         vim.cmd [[colorscheme gruvbox]]
