061bc8e20f62a64ceaf77cbbd34990f9a8e6e9e5
Huayu Zhang <levihuayuzhang@foxmail.com>
2025-04-03 14:13:51 +0800

    add back everforest theme

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -1,11 +1,12 @@
 return {
     'ellisonleao/gruvbox.nvim',
     lazy = false,
-    priority = 999,
+    priority = 1001,
     config = function()
         require('gruvbox').setup {
             contrast = 'hard', -- can be "hard", "soft" or empty string
-            transparent_mode = true,
+            -- transparent_mode = true,
+            transparent_mode = false,
         }
         vim.o.background = 'dark' -- or "light" for light mode
         vim.cmd [[colorscheme gruvbox]]
