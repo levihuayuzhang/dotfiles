48c5dc338b9375b6a88d466a599ddf4a57f6493d
Huayu Zhang <levihuayuzhang@yeah.net>
2025-04-11 20:28:25 +0800

    force to not to use arrow key
    Signed-off-by: Huayu Zhang <levihuayuzhang@yeah.net>

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -1,13 +1,15 @@
 -- use gx to open link in browser
 
 local keymap = vim.keymap
+
 -- force not using arrow keys
--- keymap.set('n', '<up>', '<nop>')
--- keymap.set('n', '<down>', '<nop>')
--- keymap.set('i', '<up>', '<nop>')
--- keymap.set('i', '<down>', '<nop>')
--- keymap.set('i', '<left>', '<nop>')
--- keymap.set('i', '<right>', '<nop>')
+keymap.set('n', '<up>', '<nop>')
+keymap.set('n', '<down>', '<nop>')
+keymap.set('i', '<up>', '<nop>')
+keymap.set('i', '<down>', '<nop>')
+keymap.set('i', '<left>', '<nop>')
+keymap.set('i', '<right>', '<nop>')
+
 keymap.set('n', '<leader>l', ':Lazy<enter>')
 keymap.set('n', '<leader>m', ':Mason<enter>')
 keymap.set('n', '<leader>e', ':Explore<enter>')
