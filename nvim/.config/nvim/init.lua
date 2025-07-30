a838114b28e6646207aa40164c16424ed699e6cf
Huayu Zhang <levihuayuzhang@yeah.net>
2025-07-30 11:59:22 +0800

    nvim: add config for typescript.
    Signed-off-by: Huayu Zhang <levihuayuzhang@yeah.net>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -249,51 +249,53 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- -- :help everforest.txt
-    -- {
-    --   "sainnhe/everforest",
-    --   -- enabled = false,
-    --   lazy = false,
-    --   priority = 1000,
-    --   config = function()
-    --     if opt.termguicolors then
-    --       -- enable 24-bit colour
-    --       opt.termguicolors = true
-    --     end
-    --
-    --     vim.g.everforest_transparent_background = 2
-    --     vim.g.everforest_float_style = "dim"
-    --     vim.g.everforest_dim_inactive_windows = 1
-    --     vim.g.everforest_diagnostic_text_highlight = 1
-    --     vim.g.everforest_diagnostic_line_highlight = 1
-    --     vim.g.everforest_diagnostic_virtual_text = "highlighted"
-    --     vim.g.everforest_inlay_hints_background = "dimmed"
-    --     vim.g.everforest_better_performance = 1
-    --     vim.g.everforest_enable_italic = 1
-    --     vim.g.everforest_disable_italic_comment = 0
-    --     vim.g.everforest_sign_column_background = "grey"
-    --     vim.g.everforest_ui_contrast = "high"
-    --     -- vim.g:everforest_background = 'soft' -- 'hard'
-    --
-    --     vim.o.background = "dark" -- or "light" for light mode
-    --     vim.cmd.colorscheme("everforest")
-    --   end,
-    -- },
-    -- -- for 256 colors terminals (not support true color)
-    -- {
-    --   "morhetz/gruvbox",
-    --   lazy = false,
-    --   config = function()
-    --     vim.cmd([[set t_Co=256]])
-    --     vim.g.gruvbox_termcolors = 256
-    --     vim.g.gruvbox_italic = 1
-    --     vim.g.gruvbox_transparent_bg = 1
-    --     vim.g.gruvbox_contrast_dark = "hard"
-    --     vim.o.background = "dark" -- or "light" for light mode
-    --     vim.cmd([[colorscheme gruvbox]])
-    --   end,
-    -- },
+    {
+      "sainnhe/everforest",
+      enabled = false,
+      lazy = false,
+      priority = 1000,
+      config = function()
+        if opt.termguicolors then
+          -- enable 24-bit colour
+          opt.termguicolors = true
+        end
+
+        vim.g.everforest_transparent_background = 2
+        vim.g.everforest_float_style = "dim"
+        vim.g.everforest_dim_inactive_windows = 1
+        vim.g.everforest_diagnostic_text_highlight = 1
+        vim.g.everforest_diagnostic_line_highlight = 1
+        vim.g.everforest_diagnostic_virtual_text = "highlighted"
+        vim.g.everforest_inlay_hints_background = "dimmed"
+        vim.g.everforest_better_performance = 1
+        vim.g.everforest_enable_italic = 1
+        vim.g.everforest_disable_italic_comment = 0
+        vim.g.everforest_sign_column_background = "grey"
+        vim.g.everforest_ui_contrast = "high"
+        -- vim.g:everforest_background = 'soft' -- 'hard'
+
+        vim.o.background = "dark" -- or "light" for light mode
+        vim.cmd.colorscheme("everforest")
+      end,
+    },
+    -- for 256 colors terminals (not support true color)
+    {
+      "morhetz/gruvbox",
+      enabled = false,
+      lazy = false,
+      config = function()
+        vim.cmd([[set t_Co=256]])
+        vim.g.gruvbox_termcolors = 256
+        vim.g.gruvbox_italic = 1
+        vim.g.gruvbox_transparent_bg = 1
+        vim.g.gruvbox_contrast_dark = "hard"
+        vim.o.background = "dark" -- or "light" for light mode
+        vim.cmd([[colorscheme gruvbox]])
+      end,
+    },
     {
       "ellisonleao/gruvbox.nvim",
+      -- enabled = false,
       lazy = false,
       priority = 1000,
       config = function()
@@ -314,6 +316,7 @@ require("lazy").setup({
     },
     {
       "nvim-lualine/lualine.nvim",
+      -- enabled = false,
       lazy = false,
       -- dependencies = { "nvim-tree/nvim-web-devicons" },
       config = function()
@@ -416,6 +419,7 @@ require("lazy").setup({
           "cmake",
           "ocamllsp",
           "vscoqtop",
+          -- "ts_ls",
         }
         vim.lsp.enable(servers)
 
@@ -1456,6 +1460,11 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons",
       }, -- if you prefer nvim-web-devicons
+      opts = {},
+    },
+    {
+      "pmizio/typescript-tools.nvim",
+      dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
       opts = {},
     },
   },
