f026054d4621ee287965e9cce640b7fc9abdf516
Huayu Zhang <zhanghuayu.dev@gmail.com>
2025-08-28 22:45:57 +0800

    switch back to transparent theme.
    Signed-off-by: Huayu Zhang <zhanghuayu.dev@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -251,35 +251,35 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- -- :help everforest.txt
-    -- {
-    --   "sainnhe/everforest",
-    --   enabled = false,
-    --   lazy = false,
-    --   priority = 1000,
-    --   config = function()
-    --     if opt.termguicolors then
-    --       -- enable 24-bit colour
-    --       opt.termguicolors = true
-    --     end
-    --
-    --     vim.g.everforest_transparent_background = 2
-    --     vim.g.everforest_float_style = "dim"
-    --     vim.g.everforest_dim_inactive_windows = 1
-    --     vim.g.everforest_diagnostic_text_highlight = 1
-    --     vim.g.everforest_diagnostic_line_highlight = 1
-    --     vim.g.everforest_diagnostic_virtual_text = "highlighted"
-    --     vim.g.everforest_inlay_hints_background = "dimmed"
-    --     vim.g.everforest_better_performance = 1
-    --     vim.g.everforest_enable_italic = 1
-    --     vim.g.everforest_disable_italic_comment = 0
-    --     vim.g.everforest_sign_column_background = "grey"
-    --     vim.g.everforest_ui_contrast = "high"
-    --     -- vim.g:everforest_background = 'soft' -- 'hard'
-    --
-    --     vim.o.background = "dark" -- or "light" for light mode
-    --     vim.cmd.colorscheme("everforest")
-    --   end,
-    -- },
+    {
+      "sainnhe/everforest",
+      -- enabled = false,
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- if opt.termguicolors then
+        -- enable 24-bit colour
+        opt.termguicolors = true
+        -- end
+
+        vim.g.everforest_transparent_background = 2
+        vim.g.everforest_float_style = "dim"
+        vim.g.everforest_dim_inactive_windows = 1
+        vim.g.everforest_diagnostic_text_highlight = 1
+        vim.g.everforest_diagnostic_line_highlight = 1
+        vim.g.everforest_diagnostic_virtual_text = "highlighted"
+        vim.g.everforest_inlay_hints_background = "dimmed"
+        vim.g.everforest_better_performance = 1
+        vim.g.everforest_enable_italic = 1
+        vim.g.everforest_disable_italic_comment = 0
+        vim.g.everforest_sign_column_background = "grey"
+        vim.g.everforest_ui_contrast = "high"
+        -- vim.g:everforest_background = "soft" -- 'hard'
+
+        vim.o.background = "dark" -- or "light" for light mode
+        vim.cmd.colorscheme("everforest")
+      end,
+    },
     -- for 256 colors terminals (not support true color)
     -- {
     --   "morhetz/gruvbox",
@@ -297,7 +297,7 @@ require("lazy").setup({
     -- },
     {
       "ellisonleao/gruvbox.nvim",
-      -- enabled = false,
+      enabled = false,
       lazy = false,
       priority = 1000,
       config = function()
