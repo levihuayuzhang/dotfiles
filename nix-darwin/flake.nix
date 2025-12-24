be788047be09ae3274bec0c30450d666417eac49
Huayu Zhang <zhanghuayu.dev@gmail.com>
2025-12-24 21:56:47 +0800

    nix-darwin: attempt tp migrate from brew to nix-darwin.
    Signed-off-by: Huayu Zhang <zhanghuayu.dev@gmail.com>

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,5 +1,8 @@
 # sudo darwin-rebuild switch --flake ~/projects/dotfiles/nix-darwin#levi-mbp
 
+# ref:
+# - https://github.com/elliottminns/nix/blob/main/flake.nix
+
 {
   description = "Example nix-darwin system flake";
 
@@ -7,13 +10,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    # # Optional: Declarative tap management
+    # # https://github.com/zhaofengli/nix-homebrew?tab=readme-ov-file#quick-start
+    # homebrew-core = {
+    #   url = "github:homebrew/homebrew-core";
+    #   flake = false;
+    # };
+    # homebrew-cask = {
+    #   url = "github:homebrew/homebrew-cask";
+    #   flake = false;
+    # };
   };
 
   outputs =
     inputs@{
       self,
-      nix-darwin,
       nixpkgs,
+      nix-darwin,
+      nix-homebrew,
+      # homebrew-core,
+      # homebrew-cask,
+      ...
     }:
     let
       configuration =
@@ -34,6 +52,12 @@
             pkgs.vim
             pkgs.neovim
             pkgs.tmux
+            pkgs.gnupg
+            pkgs.git
+            pkgs.gh
+            pkgs.eza
+            pkgs.delta
+            pkgs.bat
 
             # dev toolchains
             pkgs.cmake
@@ -47,11 +71,36 @@
             pkgs.mkalias
           ];
 
+          system.primaryUser = "zhy";
+          homebrew = {
+            enable = true;
+            brews = [
+              "zsh-autosuggestions"
+              "zsh-syntax-highlighting"
+              "mas"
+              "direnv"
+            ];
+            # brew list --cask
+            casks = [
+              # "firefox-devedition"
+              # "firefox"
+              # "mpv"
+              # "iina"
+              # "the-unarchiver"
+              "gpg-suite"
+              "hammerspoon"
+              "calibre"
+            ];
+            # # mas search xxx, App Store share - copy -> id
+            # # login & purchased
+            # masApps = {
+            #   "TestFlight" = 899247664;
+            # };
+            onActivation.cleanup = "zap";
+          };
+
           # fonts.packages = [
-          #   (pkgs.nerdfonts.override {
-          #     fonts = [ "CaskaydiaCove Nerd Font Mono" ];
-          #     # fonts = [ "JetBrainsMono" ];
-          #   })
+          #   (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
           # ];
 
           # Necessary for using flakes on this system.
@@ -76,13 +125,36 @@
 
           # The platform the configuration will be used on.
           nixpkgs.hostPlatform = "aarch64-darwin";
+
+          environment.variables = {
+            HOMEBREW_BREW_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git";
+            HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git";
+            HOMEBREW_BOTTLE_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles";
+            HOMEBREW_API_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api";
+            HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
+          };
         };
     in
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#levi-mbp
       darwinConfigurations."levi-mbp" = nix-darwin.lib.darwinSystem {
-        modules = [ configuration ];
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = false;
+              user = "zhy";
+              # autoMigrate = true;
+              # taps = {
+              #   "homebrew/homebrew-core" = homebrew-core;
+              #   "homebrew/homebrew-cask" = homebrew-cask;
+              # };
+            };
+          }
+        ];
       };
     };
 }
