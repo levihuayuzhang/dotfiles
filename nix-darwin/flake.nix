a49dabad6ecbad78dd43d31f224489e4f61d44a6
Huayu Zhang <levihuayuzhang@yeah.net>
2025-12-24 14:25:03 +0800

    nix-darwin: add mirrors.
    Signed-off-by: Huayu Zhang <levihuayuzhang@yeah.net>

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -17,24 +17,52 @@
     }:
     let
       configuration =
-        { pkgs, ... }:
+        { pkgs, config, ... }:
         {
+          nixpkgs.config.allowUnfree = true;
+
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
           environment.systemPackages = [
+            # GUI apps
+            # pkgs.ghostty # on Linux
+            # pkgs.ghostty-bin # on Mac
+            # pkgs.alacritty
+            # pkgs.obsidian # unfree
+
+            # dev tools
             pkgs.vim
             pkgs.neovim
             pkgs.tmux
-            # pkgs.ghostty # on Linux
-            pkgs.ghostty-bin # on Mac
-            pkgs.alacritty
 
+            # dev toolchains
             pkgs.cmake
+            pkgs.meson
+            pkgs.ninja
             pkgs.llvm
+
+            # beautify & misc
+            pkgs.fastfetch
+            pkgs.lolcat
+            pkgs.mkalias
           ];
+
+          # fonts.packages = [
+          #   (pkgs.nerdfonts.override {
+          #     fonts = [ "CaskaydiaCove Nerd Font Mono" ];
+          #     # fonts = [ "JetBrainsMono" ];
+          #   })
+          # ];
 
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
+
+          nix.settings.substituters = [
+            "https://mirror.sjtu.edu.cn/nix-channels/store"
+            "https://mirrors.ustc.edu.cn/nix-channels/store"
+            "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+            "https://cache.nixos.org"
+          ];
 
           # Enable alternative shell support in nix-darwin.
           # programs.fish.enable = true;
