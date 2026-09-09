b404056de2013187b5e8399adc8a33eb33dd87bc
Huayu Zhang <zhanghuayu.dev@gmail.com>
2026-09-09 22:15:05 +0800

    awww: add random gif support.
    Signed-off-by: Huayu Zhang <zhanghuayu.dev@gmail.com>

--- awww/bin/random_wallpaper.sh
+++ awww/bin/random_wallpaper.sh
@@ -6,15 +6,14 @@ WALLPAPER_DIR="$HOME/wallpapers"
 
 IMG=$(find "$WALLPAPER_DIR" -type f \( \
     -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' \
-    -o -iname '*.webp' -o -iname '*.avif' \
-\) | shuf -n 1)
+    -o -iname '*.webp' -o -iname '*.gif' -o -iname '*.avif' \
+    \) | shuf -n 1)
 
 [ -z "$IMG" ] && exit 1
 
-# pgrep -x swww-daemon >/dev/null || swww-daemon &
+# pgrep -x awww-daemon >/dev/null || awww-daemon &
 
-awww img "$IMG"
-  # --transition-type grow \
-  # --transition-fps 60 \
-  # --transition-duration 1
-
+awww img "$IMG" \
+    --transition-fps 60
+# --transition-type grow \
+# --transition-duration 1
